506edb0ba5aed8dfd3a784b0a5bb92efa4d299ee
Ubuntu <ubuntu@ip-172-31-43-134.us-west-2.compute.internal>
2017-05-18 02:37:35 +0000

    Added interpolation

--- generate_samples.py
+++ generate_samples.py
@@ -1,11 +1,14 @@
 from __future__ import print_function
-import argparse
+import argparse, iGAN_predict
 from pydoc import locate
 from lib import utils
 from lib.rng import np_rng
 import cv2
 import numpy as np
 from lib.theano_utils import floatX
+import requests
+from PIL import Image
+from StringIO import StringIO
 
 def parse_args():
     parser = argparse.ArgumentParser(description='generated random samples (dcgan_theano)')
@@ -17,6 +20,35 @@ def parse_args():
 
     args = parser.parse_args()
     return args
+
+def interpolate(url0, url1, output_image):
+    model_class = locate('model_def.dcgan_theano')
+    model_file = './models/handbag_64.dcgan_theano'
+    model = model_class.Model(
+        model_name="handbag_64", model_file=model_file)
+    # save images
+    for j, url in enumerate([url0, url1]):
+        r = requests.get(url)
+        i = Image.open(StringIO(r.content))
+        i.save("pics/url"+str(j)+".jpg")
+    z0 = iGAN_predict.find_latent(url=url0).reshape((100,))
+    z1 = iGAN_predict.find_latent(url=url1).reshape((100,))
+    delta = 1.0/32.0
+    arrays = [p*z0+(1-p)*z1 for p in np.arange(-16*delta, 1+16*delta-0.0001, delta)]
+    z = np.stack(arrays)
+    print(z.shape)
+    zmb = floatX(z[0 : 64, :])
+    xmb = model._gen(zmb)
+    samples = [xmb]
+    samples = np.concatenate(samples, axis=0)
+    print(samples.shape)
+    samples = model.inverse_transform(samples, npx=model.npx, nc=model.nc)
+    samples = (samples * 255).astype(np.uint8)
+    # generate grid visualization
+    im_vis = utils.grid_vis(samples, 8, 8)
+    # write to the disk
+    im_vis = cv2.cvtColor(im_vis, cv2.COLOR_BGR2RGB)
+    cv2.imwrite(output_image, im_vis)
 
 if __name__ == '__main__':
     args = parse_args()
@@ -55,3 +87,5 @@ if __name__ == '__main__':
     cv2.imwrite(args.output_image, im_vis)
     print('samples_shape', samples.shape)
     print('save image to %s' % args.output_image)
+
+
